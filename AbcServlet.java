8f87a9961b47a85a19396d06266ff196fffc8eb4
wirtualnarejestracja <101291057+wirtualnarejestracja@users.noreply.github.com>
2022-03-09 22:27:38 +0100

    test
    test

--- AbcServlet.java
+++ AbcServlet.java
@@ -28,8 +28,10 @@ public class AbcServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().append("Served at: ").append(request.getContextPath());
 
+            // tu blad java: cannot find symbolsymbol:   class RequestDispatcher location: class pl.nazwa.rejestracja.trzy.AbcServlet
         RequestDispatcher dispatcher = request.getRequestDispatcher("/rejestracja.jsp");
         dispatcher.forward(request, response);
+            // koniec bledu
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -43,13 +45,14 @@ public class AbcServlet extends HttpServlet {
         String contact = request.getParameter("contact");
 
         Abc abc = new Abc();
+            //poczatek bledow abc
         abc.setFirstName(firstName);
         abc.setLastName(lastName);
         abc.setUsername(username);
         abc.setPassword(password);
         abc.setContact(contact);
         abc.setAddress(address);
-
+                //koniec bledow abc
         try {
             abcdE.registerAbc(abc);
         } catch (Exception e) {
